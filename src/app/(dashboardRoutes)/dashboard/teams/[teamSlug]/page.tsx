97a02228d524fcae8c211d58297ba8395178c017
arpesh28 <arpeshgadekar@gmail.com>
2024-02-25 13:44:59 +0530

    ui: added teams design page with team cards and dropdown

---
+++ src/app/(dashboardRoutes)/dashboard/teams/[teamSlug]/page.tsx
@@ -0,0 +1,14 @@
+import MainContainer from "@/components/dashboard/mainContainer";
+import BoardView from "@/components/dashboard/tasks/boardView/taskBoardView";
+
+const Teams = () => {
+  return (
+    <MainContainer classes="flex items-start space-x-4 w-full  lg:w-[calc(100%-30px)] h-[calc(100%-156px)] lg:h-auto">
+      <BoardView title="Todo" subTitle="Some Pending Tasks" />
+      <BoardView title="Todo" subTitle="Some Pending Tasks" />
+      <BoardView title="Todo" subTitle="Some Pending Tasks" />
+    </MainContainer>
+  );
+};
+
+export default Teams;
